0be9ace4fc86e1ef17327d84942ccc7d88d2c33f
Aliaxander <admin@oxcpa.ru>
2017-05-26 21:05:11 +0300

    tst

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -1,3 +1,4 @@
 {{ id }}
-<iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%"
+
+<iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%" height="100%"
         scrolling="no"></iframe>
