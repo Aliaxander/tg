5b5074fd9e23953bb815c637d9f4105394e46ffa
Aliaxander <admin@oxcpa.ru>
2017-05-26 21:17:36 +0300

    tst

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -1,5 +1,12 @@
 <html>
-<link href="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=eGcE8daaaa)6.jpg" rel="image_src"/>
-<meta property="og:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=eGcE8daaaa)6.jpg"/>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+</style>
+<meta name="twitter:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=e0YH8daaaa)6.jpg">
+<meta name="twitter:player:width" content="640">
+<meta name="twitter:player:height" content="360">
     <iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%" height="100%" scrolling="no"></iframe>
 </html>
