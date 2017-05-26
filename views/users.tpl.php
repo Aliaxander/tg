a7e391ede033aa1bcc3907fbffd3807fe618874e
Aliaxander <admin@oxcpa.ru>
2017-05-26 21:22:48 +0300

    tst

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -8,7 +8,6 @@
 </style>
     <meta name="twitter:card" content="player">
 <meta name="twitter:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=e0YH8daaaa)6.jpg">
-    <meta name="twitter:player" content="https://www.pornhub.com/embed/ph56f3033d0a3ef">
 <meta name="twitter:player:width" content="640">
 <meta name="twitter:player:height" content="360">
 </head>
