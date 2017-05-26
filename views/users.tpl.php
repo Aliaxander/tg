f8745f6083b86feade594c750b856160c7fdcb06
Aliaxander <admin@oxcpa.ru>
2017-05-26 21:15:26 +0300

    tst

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -1,17 +1,5 @@
 <html>
 <link href="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=eGcE8daaaa)6.jpg" rel="image_src"/>
 <meta property="og:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=eGcE8daaaa)6.jpg"/>
-
-<meta name="twitter:card" content="player">
-<meta name="twitter:site" content="@pornhub">
-<meta name="twitter:creator" content="@pornhub">
-<meta name="twitter:title" content="Alexandra Tyler Playmates April 2015">
-<meta name="twitter:url"
-      content="https://www.pornhub.com/view_video.php?viewkey=ph56f3033d0a3ef&utm_source=twitter&utm_medium=social">
-<meta name="twitter:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=e0YH8daaaa)6.jpg">
-<meta name="twitter:domain" content="pornhub.com">
-<meta name="twitter:player" content="https://www.pornhub.com/embed/ph56f3033d0a3ef">
-<meta name="twitter:player:width" content="640">
-<meta name="twitter:player:height" content="360">
     <iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%" height="100%" scrolling="no"></iframe>
 </html>
