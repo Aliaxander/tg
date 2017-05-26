2fbf85de54df39e6e9e9b4d49607ac5470c8b8ed
Aliaxander <admin@oxcpa.ru>
2017-05-26 21:20:47 +0300

    tst

--- views/users.tpl.php
+++ views/users.tpl.php
@@ -1,12 +1,18 @@
 <html>
+<head>
 <style>
     * {
         margin: 0;
         padding: 0;
     }
 </style>
+    <meta name="twitter:card" content="player">
 <meta name="twitter:image" content="https://bi.phncdn.com/videos/201603/23/71843132/original/(m=e0YH8daaaa)6.jpg">
+    <meta name="twitter:player" content="https://www.pornhub.com/embed/ph56f3033d0a3ef">
 <meta name="twitter:player:width" content="640">
 <meta name="twitter:player:height" content="360">
-    <iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%" height="100%" scrolling="no"></iframe>
+</head>
+<body>
+<iframe src="http://www.pornhub.com/embed/{{ id }}" frameborder="0" width="100%" height="100%" scrolling="no"></iframe>
+</body>
 </html>
