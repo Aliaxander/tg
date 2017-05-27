08550cecbea5723c2f9ef520e2cfb0a5496142d6
Aliaxander <admin@oxcpa.ru>
2017-05-27 23:44:18 +0300

    tst

--- OxApp/controllers/WebhookController.php
+++ OxApp/controllers/WebhookController.php
@@ -61,10 +61,6 @@ class WebhookController extends App
                 ]);
                 $user = Users::find(['chatId' => $chatId])->rows[0];
             }
-            print_r($telegram->sendMessage([
-                'chat_id' => $chatId,
-                'text' => json_encode($userData)
-            ]));
         }
         $user = Users::find(['chatId' => $chatId])->rows[0];
         Users::where(['id' => $user->id])->update(['requests' => $user->requests + 1]);
